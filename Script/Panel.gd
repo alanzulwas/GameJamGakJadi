c8410951317701134a5aee63aeaa5e46c92e82bb
KonoTabi <36232476+alanzulwas@users.noreply.github.com>
2022-03-18 22:55:10 +0800

    v.0
    ya gitu la

--- Script/Panel.gd
+++ Script/Panel.gd
@@ -1,9 +1,21 @@
 extends ColorRect
 
+var _kotak
+
+var _invetory = load("res://Scene/Inventory.tscn")
+
+func _initIsi():
+	pass
+
 func _ready():
+	_initIsi()
 	pass # Replace with function body.
 
-
+func _process(delta):
+	if self.is_visible():
+		print(_kotak)
+		pass
+	pass
 
 func _on_Button_pressed():
 	set_visible(false)
--- Script/click me.gd
+++ Script/click me.gd
@@ -1,23 +1,36 @@
 extends Area2D
 
+var _inventory
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Input.set_custom_mouse_cursor(cursor.cursor1, Input.CURSOR_ARROW, Vector2(16,16))
 
+func _clicked():
+	var kotak = str(self)
+	if kotak == "Area2D1:[Area2D:1252]" :
+		get_node("/root/Scene/Panel/ColorRect")._kotak = "kotak1"
+	elif kotak == "Area2D2:[Area2D:1254]" :
+		get_node("/root/Scene/Panel/ColorRect")._kotak = "kotak2"
+	elif kotak == "Area2D3:[Area2D:1256]" :
+		get_node("/root/Scene/Panel/ColorRect")._kotak = "kotak3"
+	else :
+		print("tidak")
+
 func _on_Area2D_input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton:
-		if event.is_pressed():
-			Input.set_custom_mouse_cursor(cursor.cursor3, Input.CURSOR_ARROW, Vector2(16,16))
-			print("press")
-			get_node("/root/Scene/Panel/ColorRect").set_visible(true)
-		else:
-			Input.set_custom_mouse_cursor(cursor.cursor2, Input.CURSOR_ARROW, Vector2(16,16))
-			print("unpressed")
+	if !get_node("/root/Scene/Panel/ColorRect").is_visible():
+		if event is InputEventMouseButton:
+			if event.is_pressed():
+				Input.set_custom_mouse_cursor(cursor.cursor3, Input.CURSOR_ARROW, Vector2(16,16))
+				_clicked()
+				get_node("/root/Scene/Panel/ColorRect").set_visible(true)
+			else:
+				Input.set_custom_mouse_cursor(cursor.cursor2, Input.CURSOR_ARROW, Vector2(16,16))
 
 func _on_Area2D_mouse_entered():
-	Input.set_custom_mouse_cursor(cursor.cursor2, Input.CURSOR_ARROW, Vector2(16,16))
-	print("enter")
+	if !get_node("/root/Scene/Panel/ColorRect").is_visible():
+		Input.set_custom_mouse_cursor(cursor.cursor2, Input.CURSOR_ARROW, Vector2(16,16))
 
 func _on_Area2D_mouse_exited():
-	Input.set_custom_mouse_cursor(cursor.cursor1, Input.CURSOR_ARROW, Vector2(16,16))
-	print("exit")
+	if !get_node("/root/Scene/Panel/ColorRect").is_visible():
+		Input.set_custom_mouse_cursor(cursor.cursor1, Input.CURSOR_ARROW, Vector2(16,16))
